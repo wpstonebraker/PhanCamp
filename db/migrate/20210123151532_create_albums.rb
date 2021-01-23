ec6e7e308d38db6acc41e50d0facf578dd0b737b
Paul Stonebraker <wpstonebraker@gmail.com>
2021-01-23 12:21:08 -0500

    create artist model

---
+++ db/migrate/20210123151532_create_albums.rb
@@ -0,0 +1,16 @@
+class CreateAlbums < ActiveRecord::Migration[5.2]
+  def change
+    create_table :albums do |t|
+      t.string :title, null: false
+      t.integer :artist_id, null: false
+      t.date :release_date, null: false
+      t.integer :price, null: false, default: 0
+      t.text :description
+      t.text :credits, null: false
+      
+
+      t.timestamps
+    end
+    add_index :albums, [:title, :artist_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_202503) do
+ActiveRecord::Schema.define(version: 2021_01_23_151532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2021_01_21_202503) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "artist_id", null: false
+    t.date "release_date", null: false
+    t.integer "price", default: 0, null: false
+    t.text "description"
+    t.text "credits", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title", "artist_id"], name: "index_albums_on_title_and_artist_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
