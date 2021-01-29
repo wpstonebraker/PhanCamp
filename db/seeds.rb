170d45926763a06f48c0148d159edc5d3d7c3a48
Paul Stonebraker <wpstonebraker@gmail.com>
2021-01-29 01:43:17 -0500

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ u1 = User.create!(
     username: "Guest",
     password: "misterhiro",
     email: "hiro@bestdog.com",
-    description: "Mister Hiro was the most interesting dog in the world.",
+    about: "Mister Hiro was the most interesting dog in the world.",
 )
 
 u1.banner.attach(io: File.open("app/assets/images/thumb.png"), filename: "guestthumb.png")
